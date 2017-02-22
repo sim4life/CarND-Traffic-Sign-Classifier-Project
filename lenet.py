608ff47aa1d34601fdb42602955ae5021636a8c2
Shah Ismail <sim4life@yahoo.com>
2017-02-22 15:48:26 +0000

    Added lenet py

---
+++ lenet.py
@@ -0,0 +1,137 @@
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True, reshape=False)
+
+import tensorflow as tf
+
+# Parameters
+LEARNING_RATE = 0.00001
+EPOCHS = 10
+BATCH_SIZE = 128
+
+# Number of samples to calculate validation and accuracy
+# Decrease this if you're running out of memory to calculate accuracy
+test_valid_size = 256
+
+# Network Parameters
+n_classes = 10  # MNIST total classes (0-9 digits)
+dropout = 0.75  # Dropout, probability to keep units
+
+
+# Store layers weight & bias
+weights = {
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))}
+
+
+def conv2d(x, W, b, strides=1):
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+def maxpool2d(x, k=2):
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+
+def conv_net(x, weights, biases, dropout):
+    # Layer 1 - 28*28*1 to 14*14*32
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Layer 2 - 14*14*32 to 7*7*64
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer - 7*7*64 to 1024
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output Layer - class prediction - 1024 to 10
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)
+
+# Model
+logits = conv_net(x, weights, biases, keep_prob)
+
+# Define loss and optimizer - cost is also loss_op, optimizer is also train_op
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+opt = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE)
+# opt = tf.train.AdamOptimizer()
+optimizer = opt.minimize(cost)
+
+# Accuracy - accuracy is also accuracy_op
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+def eval_data(dataset):
+    """
+    Given a dataset as input returns the loss and accuracy.
+    """
+    steps_per_epoch = dataset.num_examples // BATCH_SIZE
+    num_examples = steps_per_epoch * BATCH_SIZE
+    total_acc, total_loss = 0, 0
+    sess = tf.get_default_session()
+    for step in range(steps_per_epoch):
+        batch_x, batch_y = dataset.next_batch(BATCH_SIZE)
+        loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y})
+        total_acc += (acc * batch_x.shape[0])
+        total_loss += (loss * batch_x.shape[0])
+    return total_loss/num_examples, total_acc/num_examples
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    steps_per_epoch = mnist.train.num_examples // BATCH_SIZE
+
+    for epoch in range(EPOCHS):
+        for batch in range(steps_per_epoch):
+            batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: dropout})
+
+            # Calculate batch loss and accuracy
+            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.})
+            valid_acc = sess.run(accuracy, feed_dict={
+                x: mnist.validation.images[:test_valid_size],
+                y: mnist.validation.labels[:test_valid_size],
+                keep_prob: 1.})
+
+            print('Epoch {:>2}, Batch {:>3} - Loss: {:>10.4f} Validation Accuracy: {:.6f}'.format(
+                epoch + 1,
+                batch + 1,
+                loss,
+                valid_acc))
+
+        val_loss, val_acc = eval_data(mnist.validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation loss = {:.3f}".format(val_loss))
+        print("Validation accuracy = {:.3f}".format(val_acc))
+        print()
+
+
+    # Calculate Test Accuracy
+    test_acc = sess.run(accuracy, feed_dict={
+        x: mnist.test.images[:test_valid_size],
+        y: mnist.test.labels[:test_valid_size],
+        keep_prob: 1.})
+    print('Testing Accuracy with prob: {}'.format(test_acc))
+
+    # Evaluate on the test data
+    test_loss, test_acc = eval_data(mnist.test)
+    print("Eval_func Test loss = {:.3f}".format(test_loss))
+    print("Eval_func Test accuracy = {:.3f}".format(test_acc))
