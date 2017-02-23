22b34681518ca3989758ebe6248c9425be8d9fef
Shah Ismail <sim4life@yahoo.com>
2017-02-23 10:35:09 +0000

    SGD decayLr 7

--- lenet.py
+++ lenet.py
@@ -109,6 +109,10 @@ biases = {
 
 
 def decay_learning_rate(size):
+    divisor = 1000
+    if size > 6:
+        divisor = 10000
+    d = 1/divisor
     return 1/(1000*size) # return 1/(10*(size**math.e)) # return 1/(2*(math.e**size))
 def conv2d(x, W, b, strides=1):
     x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='VALID')
@@ -168,10 +172,10 @@ logits = LeNet(x, weights, biases, keep_prob)
 
 # Define loss (cost) and optimizer (training_operation)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y))
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=decaying_learning_rate)
-# model_save_dir = 'LeNet_model_sgd'
-optimizer = tf.train.AdamOptimizer(learning_rate=decaying_learning_rate)
-model_save_dir = 'LeNet_model_adam'
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=decaying_learning_rate)
+model_save_dir = 'LeNet_model_sgd'
+# optimizer = tf.train.AdamOptimizer(learning_rate=decaying_learning_rate)
+# model_save_dir = 'LeNet_model_adam'
 training_operation = optimizer.minimize(cost)
 
 # Accuracy (accuracy_operation)
