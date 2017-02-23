5df78a04b71818139525e18433b9ad7053bf7c53
Shah Ismail <sim4life@yahoo.com>
2017-02-23 11:17:09 +0000

    Adam decayLr 16

--- lenet.py
+++ lenet.py
@@ -113,8 +113,8 @@ def decay_learning_rate(size):
     if size > 8:
         # divisor = 1000*size
         # divisor = math.e**size
-        divisor = 10*(size**math.e)
-        # divisor = size**math.e
+        # divisor = 10*(size**math.e)
+        divisor = size**math.e
     return 1/divisor
     # return 1/(10*(size**math.e)) # return 1/(2*(math.e**size))
 def conv2d(x, W, b, strides=1):
