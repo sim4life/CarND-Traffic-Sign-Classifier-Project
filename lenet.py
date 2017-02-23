c6abc9c2661c3bef008a7ac24f7052cef7af59b7
Shah Ismail <sim4life@yahoo.com>
2017-02-23 14:37:24 +0000

    norm+eq

--- lenet.py
+++ lenet.py
@@ -135,8 +135,8 @@ def preprocess(imgs):
     print("a3 normalize np.max / np.min::imgs.mean:{} / {}::{}".format(np.max(new_imgs), np.min(new_imgs), new_imgs.mean()))
     new_imgs = equalize(new_imgs)
     print("a3 equalize np.max / np.min::imgs.mean:{} / {}::{}".format(np.max(new_imgs), np.min(new_imgs), new_imgs.mean()))
-    new_imgs = rgb2gray(new_imgs)
-    print("a3 rgb2gray np.max / np.min::imgs.mean:{} / {}::{}".format(np.max(new_imgs), np.min(new_imgs), new_imgs.mean()))
+    # new_imgs = rgb2gray(new_imgs)
+    # print("a3 rgb2gray np.max / np.min::imgs.mean:{} / {}::{}".format(np.max(new_imgs), np.min(new_imgs), new_imgs.mean()))
     return rgb2gray(new_imgs)
 
 # preprocess the images
