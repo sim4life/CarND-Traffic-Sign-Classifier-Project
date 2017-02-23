c57e2682b1ca0aace1f4ca482eaa736a454de81d
Shah Ismail <sim4life@yahoo.com>
2017-02-23 09:51:52 +0000

    Adam decayLr 2

--- lenet.py
+++ lenet.py
@@ -167,10 +167,10 @@ logits = LeNet(x, weights, biases, keep_prob)
 
 # Define loss (cost) and optimizer (training_operation)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=decaying_learning_rate)
-model_save_dir = 'LeNet_model_sgd'
-# optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-# model_save_dir = 'LeNet_model_adam'
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=decaying_learning_rate)
+# model_save_dir = 'LeNet_model_sgd'
+optimizer = tf.train.AdamOptimizer(learning_rate=decaying_learning_rate)
+model_save_dir = 'LeNet_model_adam'
 training_operation = optimizer.minimize(cost)
 
 # Accuracy (accuracy_operation)
